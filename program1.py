381cda40fa1dcff33d8ebb9e791eb44d7778c32c
gunnu15 <sgfashionart@gmail.com>
2024-12-26 21:41:23 +0530

    Add files via upload

---
+++ program1.py
@@ -0,0 +1,10 @@
+string = input("Enter a word:")
+char = input("Enter a character:")
+i = 0
+count = 0
+while(i < len(string)):
+    if (string[i] == char):
+        count = count + 1
+    i = i + 1
+
+print("The total number of times", char,"has occured =", count)
