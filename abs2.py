986361f61dee2f33b9c4092e78ba5d00bf4b8332
gunnu15 <sgfashionart@gmail.com>
2025-05-10 17:48:08 +0530

    Add files via upload

---
+++ abs2.py
@@ -0,0 +1,14 @@
+class square:
+    def __init__(self, side):
+        self.side = side
+    def area(self):
+        print("My area is :", self.side**2)
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+    def area(self):
+        print("My area is :", 3.14*self.radius*self.radius)
+osquare = square(5)
+ocircle = Circle(5)
+for shape in(osquare, ocircle):
+    shape.area()
